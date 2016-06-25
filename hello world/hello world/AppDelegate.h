47e47c02ca78a47c3ce84a6d4d528df42857475f
qjx <qjx>
2016-06-25 10:10:45 +0800

    初始化项目

---
+++ hello world/hello world/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello world
+//
+//  Created by 权建星 on 16/6/25.
+//  Copyright © 2016年 权建星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
